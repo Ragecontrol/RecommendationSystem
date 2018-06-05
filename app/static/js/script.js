c97260cabb4879bdc47984febdd52ee11568a6e3
Ragecontrol <maximandreev2015@yandex.ru>
2018-06-05 21:24:30 +0300

    update

---
+++ app/static/js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+$('#star-10{{item.id}}').click(function(){
+    alert ('10')});
+});
+
+$('#star-9').click(function(){
+    alert ('9');
+});
+
+$('#star-8').click(function(){
+    alert ('8');
+});
+
+$('#star-7').click(function(){
+    alert ('7');
+});
+
+$('#star-6').click(function(){
+    alert ('6');
+});
+
+$('#star-5').click(function(){
+    alert ('5');
+});
+
+$('#star-4').click(function(){
+    alert ('4');
+});
+
+$('#star-3').click(function(){
+    alert ('3');
+});
+
+$('#star-2').click(function(){
+    alert ('2');
+});
+
+$('#star-1').click(function(){
+    alert ('1');
+});
