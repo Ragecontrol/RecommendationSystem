4eba2124f58c70faf63058e4922671424dfbaeba
Ragecontrol <maximandreev2015@yandex.ru>
2018-06-11 19:02:40 +0300

    update

--- config.py
+++ config.py
@@ -4,9 +4,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FILM_PER_PAGE = 5
     POSTS_PER_PAGE = 15
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = 1
+    MAIL_USERNAME = 'servakonly@gmail.com'
+    MAIL_PASSWORD = 'memasiki123'
+    ADMINS = ['servakonly@gmail.com']
